2895a6a4a65cacd249e6eb650a8cd07ef7a6e9d0
Kevsely <elegis.sossou@yahoo.com>
2022-02-14 07:09:54 +0100

    Listing element non affected by the automatic ...theme switch

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let themeSwitchButton = document.querySelector("[type=checkbox]");
+const themeSwitchButton = document.querySelector("[type=checkbox]");
+
+const navbar = document.getElementById("nav");
+const image1 = document.getElementById("image1");
+const image1 = document.getElementById("image2");
+const image1 = document.getElementById("image3");
+const textbox = document.getElementById("text-box");
 
 // Change root custom attribute 'data-theme'
 function changeTheme(event) {
     console.log(event.target.checked);
     if(event.target.checked)
+    {
         document.documentElement.setAttribute("data-theme", "dark");
+    }
     else
+    {
         document.documentElement.setAttribute("data-theme", "light");
+    }
 }
 
 // Event listener
