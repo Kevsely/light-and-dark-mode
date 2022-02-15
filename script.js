b2c32fb84acc0791994868f506817e408f2b63d4
Kevsely <elegis.sossou@yahoo.com>
2022-02-15 06:35:03 +0100

    Removing old unuseful function

--- script.js
+++ script.js
@@ -8,28 +8,6 @@ const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 
-let currentMode = "Light";
-
-function setToDark() {
-    themeText.textContent = "Dark Mode";
-    themeLogo.classList.replace("fa-sun", "fa-moon");
-    navbar.style.backgroundColor = "rgb(0 0 0 / 50%)";
-    textbox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-    image1.src = "img/undraw_proud_coder_dark.svg";
-    image2.src = "img/undraw_feeling_proud_dark.svg";
-    image3.src = "img/undraw_conceptual_idea_dark.svg";
-}
-
-function setToLight() {
-    themeText.textContent = "Light Mode";
-    themeLogo.classList.replace("fa-moon", "fa-sun");
-    navbar.style.backgroundColor = "rgb(255 255 255 / 50%)";
-    textbox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-    image1.src = "img/undraw_proud_coder_light.svg";
-    image2.src = "img/undraw_feeling_proud_light.svg";
-    image3.src = "img/undraw_conceptual_idea_light.svg";
-}
-
 function setTheme (isDark) {
     themeText.textContent = (isDark ? "Dark" : "Light") + " Mode";
     themeLogo.classList.toggle("fa-moon");
