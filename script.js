cb97cbfe8ad6f358e543bc9dbb9af62df43b8927
Kevsely <elegis.sossou@yahoo.com>
2022-02-15 06:12:30 +0100

    Filling the setToDark Function

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const themeSwitchButton = document.querySelector("[type=checkbox]");
 
-const navbar = document.getElementById("nav");
-const image1 = document.getElementById("image1");
-const image1 = document.getElementById("image2");
-const image1 = document.getElementById("image3");
-const textbox = document.getElementById("text-box");
 const themeText = document.getElementById("toggle-text");
+const themeLogo = document.getElementsByClassName("fas")[0];
+const navbar = document.getElementById("nav");
+const textbox = document.getElementById("text-box");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
 
 function setToDark() {
-
+    themeText.textContent = "Dark Mode";
+    themeLogo.classList.replace("fa-sun", "fa-moon");
+    navbar.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    textbox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    image1.src = "img/undraw_proud_coder_dark.svg";
+    image2.src = "img/undraw_feeling_proud_dark.svg";
+    image3.src = "img/undraw_conceptual_idea_dark.svg";
 }
 
 function setToLight() {
